0fd2e5a5c809e66f08fd3eccea8880602e2961be
megecane <isimege-community@discord.com>
2021-12-23 10:45:06 +0100

    '.'

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
   name = 'sessionOffedul',         # How you named your package folder (MyLib)
   packages = ['sessionOffedul'],   # Chose the same as "name"
-  version = '0.3',      # Start with a small number and increase it with every change you make
+  version = '0.4',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'This package supports flask and it is easy to use, it has the main scope to make Flask Session Managment more easy and customizable!',   # Give a short description about your library
   author = 'OffedulDev',                   # Type in your name
   author_email = 'aloneforever.commercial@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/OffedulDev/session',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/OffedulDev/session/archive/refs/tags/v_01.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/OffedulDev/session/archive/refs/tags/v_02.tar.gz',    # I explain this later on
   keywords = ['HELPFUL', 'EASY', 'CUSTOMIZABLE', 'FLASK'],   # Keywords that define your package best
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
