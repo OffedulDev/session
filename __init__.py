d205a19ef43c85623e3ba2fc7bfe468f75854e93
megecane <isimege-community@discord.com>
2021-12-23 10:18:04 +0100

    added __init__.py file to support PIP packages

---
+++ __init__.py
@@ -0,0 +1,4 @@
+from sessionOffedul.objectmanager import (IntegerObject, StringObject, TupleObject, ListObject, 
+	UnknownObject, GetSession, UpdateSession, PrintContentOnFile, ConvertJsonToObject, AddJson, GetObject,
+	DeleteObject, Object, InitializeSession, GetJson)
+
